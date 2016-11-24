474020f8f76cf78268c0a01d088fb028a3cbf37e
José Rodrigues <jrstylletecnologia@hotmail.com>
2016-11-24 15:29:05 -0200

    Atualização do arquivo, remoção do popup
    Desativei o pop na área administrativa e exclui os posts específicos do if, deixando as penas a função is_home() caso precise ativar o popup novamente.

--- wp-content/themes/rcam_v1-0/advertising-popup.php
+++ wp-content/themes/rcam_v1-0/advertising-popup.php
@@ -1,21 +1,6 @@
 <?php 
-	if(
-        is_home() 
-        || is_single('Caminhoneiro sofre com estereótipos, quando deveria ser o herói da história')/* 18/11 */
-        || is_single('Aprenda a montar seu currículo para conquistar um emprego de caminhoneiro')/* 19/11 */
-        || is_single('O caminhão com turbinas mais rápido do mundo')/* 19/11 */
-        || is_single('Churrascada! Quem não gosta?')/* 20/11 */
-        || is_single('Os caminhoneiros estão mais jovens')/* 20/11 */
-        || is_single('Vencemos')/* 21/11 */
-        || is_single('A lenda vive')/* 21/11 */
-        || is_single('sentimentos opostos')/* 22/11 */
-        || is_single('Assim nasce um caminhoneiro')/* 23/11 */
-        || is_single('25 frases de caminhoneiro encontradas nas estradas brasileiras')/* 23/11 */
-        || is_single('O REI DO GADO')/* 24/11 */
-        || is_single('Temos futuro! Mas…..')/* 24/11 */
-        || is_single('14 filmes de caminhoneiro imperdíveis')/* 25/11 */
-        || is_single('AS 14 ESTRADAS MAIS PERIGOSAS DO MUNDO')/* 25/11 */
-    ){
+	if( is_home() ){
+        
 		if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-13'));
 	}
 
